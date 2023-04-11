60f51c69af8f0f709f7aaff5bdaeb3f47b963d5e
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-04-11 17:28:26 +0100

    added add-property__city

--- src/components/AddProperty.jsx
+++ src/components/AddProperty.jsx
@@ -5,6 +5,7 @@ const AddProperty = () => {
   const initialState = {
     fields: {
       title: '',
+      city: '',
     },
   };
   const [fields, setFields] = useState(initialState.fields);
@@ -26,15 +27,38 @@ const AddProperty = () => {
     <div className="add-property">
       <h2>Add Property Page</h2>
       <form className="add-property__form" onSubmit={handleAddProperty}>
-        <input
-          type="text"
-          className="add-property__form-text"
-          value={fields.title}
-          id="add-property__title"
-          onChange={handleFieldChange}
-          name="title"
-        />
-        <button type="submit">Add</button>
+        <label htmlFor="add-property__title">
+          <input
+            type="text"
+            className="add-property__form-text"
+            value={fields.title}
+            id="add-property__title"
+            onChange={handleFieldChange}
+            name="title"
+          />
+        </label>
+        <label htmlFor="add-property__city">
+          <select
+            name="city"
+            id="add-property__city"
+            onChange={handleFieldChange}
+            value={fields.city}
+          >
+            <option value="" disabled selected>
+              -- CHOOSE CITY --
+            </option>
+            <option value="London">London</option>
+            <option value="Birmingham">Birmingham</option>
+            <option value="Glasgow">Glasgow</option>
+            <option value="Liverpool">Liverpool</option>
+            <option value="Bristol">Bristol</option>
+            <option value="Manchester">Manchester</option>
+            <option value="Sheffield">Sheffield</option>
+          </select>
+        </label>
+        <button className="add-property__form-btn" type="submit">
+          Add
+        </button>
       </form>
     </div>
   );
