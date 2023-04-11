f36d70b8b863ef8695e6abce514cc5bfb16df092
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-04-11 12:06:35 +0100

    fixed elint error and added navbar routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import '../styles/app.css';
+import AddProperty from './AddProperty';
 import Navbar from './Navbar';
+import Properties from './Properties';
 
 const App = () => (
   <BrowserRouter>
     <Navbar />
     <Routes>
-      <Route />
+      <Route path="/" element={<Properties />} />
+      <Route path="add-property" element={<AddProperty />} />
     </Routes>
   </BrowserRouter>
 );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 import { ReactComponent as Castle } from '../assets/castle-logo.svg';
 
@@ -9,8 +10,13 @@ const Navbar = () => (
         <Castle color="purple" className="navbar-logo" />
         <h2 className="navbar-logo-text">Surreal Estate</h2>
       </div>
-      <h2 className="navbar-links-item">View Properties</h2>
-      <h2 className="navbar-links-item">Add a Property</h2>
+
+      <Link to="/">
+        <h2 className="navbar-links-item">View Properties</h2>
+      </Link>
+      <Link to="/add-property">
+        <h2 className="navbar-links-item">Add a Property</h2>
+      </Link>
     </div>
   </nav>
 );
