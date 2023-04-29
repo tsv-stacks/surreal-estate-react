a8cfaa0df437f554d0811ed3aaedad0d47f8e9d3
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-04-29 01:32:16 +0100

    added search bar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,34 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/Sidebar.css';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { ReactComponent as Search } from '../assets/search.svg';
 
 const qs = require('qs');
 
 const SideBar = ({ uniqueCityArray }) => {
-  console.log('sidebar', uniqueCityArray);
+  const location = useLocation();
+  const navigate = useNavigate();
 
   const [propertiesArray, setPropertiesArray] = useState([]);
-  const [selectedCities, setSelectedCities] = useState([]);
+  const [query, setQuery] = useState('');
+  // const [selectedCities, setSelectedCities] = useState([]);
 
   useEffect(() => {
     setPropertiesArray(uniqueCityArray);
   }, [uniqueCityArray]);
 
-  const handleChange = (event) => {
-    const { value, checked, name } = event.target;
-    console.log(value, checked, name);
-    setSelectedCities((prevState) => {
-      if (prevState.includes(name)) {
-        return prevState.filter((city) => city !== name);
-      }
-      return [...prevState, name];
-    });
-    console.log(selectedCities);
-  };
+  // const handleChange = (event) => {
+  //   const { value, checked, name } = event.target;
+  //   console.log(value, checked, name);
+  //   setSelectedCities((prevState) => {
+  //     if (prevState.includes(name)) {
+  //       return prevState.filter((city) => city !== name);
+  //     }
+  //     return [...prevState, name];
+  //   });
+  //   console.log(selectedCities);
+  // };
 
   const buildQueryString = (operation, valueObj) => {
-    const { search } = useLocation();
+    const { search } = location;
     const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+    console.log(currentQueryParams);
     const newQueryParams = {
       ...currentQueryParams,
       [operation]: JSON.stringify(valueObj),
@@ -38,33 +42,55 @@ const SideBar = ({ uniqueCityArray }) => {
       encode: false,
     });
   };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const newQueryString = buildQueryString('query', {
+      title: { $regex: query },
+    });
+    console.log(newQueryString);
+    navigate(newQueryString);
+  };
+
   return (
     <nav className="sidebar">
+      <form className="sidebar-text-search" onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+          placeholder="Search by title..."
+        />
+        <button type="submit">
+          <Search />
+        </button>
+      </form>
+
       <h3 className="sidebar-city__title">Filter by city:</h3>
       <form>
         {propertiesArray.length > 0 && (
           <div className="sidebar-city">
             {propertiesArray.map((sideCity) => (
-              <Link
-                className="sidebar-city__link"
+              <label
                 key={sideCity}
-                to={buildQueryString('query', { city: `${sideCity}` })}
+                className="sidebar-city__label"
+                htmlFor={`sidebar-city__input-${sideCity}`}
               >
-                <label
-                  className="sidebar-city__label"
-                  htmlFor={`sidebar-city__input-${sideCity}`}
+                <Link
+                  className="sidebar-city__link"
+                  to={buildQueryString('query', { city: `${sideCity}` })}
                 >
-                  {sideCity}
                   <input
                     type="checkbox"
                     name={`${sideCity}`}
                     id={`sidebar-city__input-${sideCity}`}
                     className="sidebar-city__input"
-                    onChange={handleChange}
-                    checked={selectedCities.includes(sideCity)}
+                    // onChange={handleChange}
+                    defaultChecked={false}
                   />
-                </label>
-              </Link>
+                  <span className="sidebar-city__name">{sideCity}</span>
+                </Link>
+              </label>
             ))}
           </div>
         )}
